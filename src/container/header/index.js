e7029530476447aa5e27241554a1bc7d8e0e5b7f
RewDatNeew <oloandyolo@gmail.com>
2020-12-28 20:55:50 +0300

    хедер

---
+++ src/container/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.less';
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <div className="title-header">
+                Who Cares About Ur Project?
+            </div>
+        </div>
+    )
+}
--- src/container/index.js
+++ src/container/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './style.less';
+import { Header } from "./header";
 
 export const AppContainer = () => {
   return (
     <div className="AppContainer">
-      sdfsd
+      <Header />
     </div>
   );
 }
