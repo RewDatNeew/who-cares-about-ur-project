1d9029196b85ae0c96ded9085adeea7ccb50db6f
RewDatNeew <oloandyolo@gmail.com>
2021-01-13 22:55:13 +0300

    1api

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,4 @@
 export const PROJECT = 'https://my-json-server.typicode.com/RewDatNeew/data-json'
-export const API = 'https://mockend.com/RewDatNeew/who-cares-about-ur-project'
 
 export const USERS = `${PROJECT}/users`;
 
